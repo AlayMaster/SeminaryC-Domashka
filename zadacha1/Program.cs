9bdeb6b5e144b3a82629c9677fae24568a99e4b7
Alay <alexeypekin13@gmail.com>
2022-06-24 23:10:18 +0300

    первая задача

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -1,4 +1,21 @@
 ﻿// Задача 1
+int num1, num2;
 
+Console.Write("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Hello, World!");
+if (num1 > num2)
+{
+    Console.WriteLine("Большее число" + num1 );
+}
+if (num1 < num2)
+{
+    Console.WriteLine("Большее число" + num2 );
+}
+if (num1 == num2)
+{
+    Console.WriteLine("Вы ввели равные числа");
+}
+
