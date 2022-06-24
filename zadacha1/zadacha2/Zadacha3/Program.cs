e0a6202ec4d67a540fe318103139a7097ac85122
Alay <alexeypekin13@gmail.com>
2022-06-25 00:56:50 +0300

    задача3

---
+++ zadacha1/zadacha2/Zadacha3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача3
+int num;
+
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine(num + " - Это число четче некуда");
+}
+else
+{
+    Console.WriteLine(num + " - Число совсем не четное");
+}
+
