8cb0ae8789c669bc9f053fba5468753c575cfdba
Alay <alexeypekin13@gmail.com>
2022-06-25 16:20:41 +0300

    задача4

---
+++ zadacha1/zadacha2/Zadacha3/zadacha4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 4
+int num,current;
+
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+for (current = 1; current <= num; current++)
+  if(current % 2 == 0)
+  {
+    Console.Write(current + " ");
+    current++;
+  }
+
+ 
