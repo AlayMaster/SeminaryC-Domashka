a0c65bb6fd0f61ce6699da28bb97fad8bc8ce6c2
Alay <alexeypekin13@gmail.com>
2022-07-01 01:56:23 +0300

    1

--- Domashka3Sem/Program.cs
+++ Domashka3Sem/Program.cs
@@ -1,16 +1,17 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
-void Cube(int x)
+/*
+void Quad(int x)
 {
     int current = 1;
     while (current <= x)
     {
-        int cube = current * current * current;
-        Console.Write(cube + " ");
+        int quad = current * current;
+        Console.Write(quad + " ");
         current++;
     }
 }
 
 Console.WriteLine("Input a number: ");
 int n = Convert.ToInt32(Console.Readline());
-Cube (n);
+Quad (n);
+*/
