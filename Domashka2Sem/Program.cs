c18d1b059ca8ea08626b9f024a4bf59a6cd10d16
Alay <alexeypekin13@gmail.com>
2022-07-01 02:03:53 +0300

    domashka2

---
+++ Domashka2Sem/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int DoubleNumber()
+{
+    int num = new Random().Next(100,1000);
+    Console.WriteLine("Random number is: " + num);
+    int des = (num / 10) % 10;
+    return des;
+}
+
+int number = DoubleNumber();
+Console.WriteLine("Double number is: " + number);
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
