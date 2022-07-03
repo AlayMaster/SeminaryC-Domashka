865e1423c37b8df27556cd88834a899797f50379
Alay <alexeypekin13@gmail.com>
2022-07-03 19:23:46 +0300

    4

--- Domashka2Sem/Program.cs
+++ Domashka2Sem/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-/*
+
 int DoubleNumber()
 {
     int num = new Random().Next(100,1000);
@@ -11,7 +11,7 @@ int DoubleNumber()
 int number = DoubleNumber();
 Console.WriteLine("Double number is: " + number);
 
-*/
+
 //Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 void Week(int x)
@@ -23,3 +23,28 @@ void Week(int x)
 Console.WriteLine("Input number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Week(num);
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int ThirdNumber()
+{
+    Console.WriteLine("Input number: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num < 100) Console.WriteLine("третьего числа нет");
+    if (num > 999 && num <=9999)
+        num = num / 10;
+        int res = num % 10;
+  /*   while (num>=9999)  // чет я голову ломал,так и не додумался как порядок понижать до этих трех цифр
+    {
+        int k=10;
+        num = num / 10*k;
+        int res2 = num % 10;
+        return res2;
+    }
+    */
+     
+    return res; 
+}
+
+int number = ThirdNumber();
+Console.WriteLine("Third number is: " + number);
