f1dceda5b987cb319346e902e98540e8cd15c788
Alay <alexeypekin13@gmail.com>
2022-07-03 14:58:42 +0300

    3

--- Domashka2Sem/Program.cs
+++ Domashka2Sem/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int DoubleNumber()
 {
     int num = new Random().Next(100,1000);
@@ -11,4 +11,15 @@ int DoubleNumber()
 int number = DoubleNumber();
 Console.WriteLine("Double number is: " + number);
 
+*/
 //Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Week(int x)
+{
+    if (x >= 1 && x <=5 ) Console.WriteLine("No");
+    if (x >=6 && x <=7) Console.WriteLine("Yes");
+    if (x >= 8) Console.WriteLine("Number is uncorrect");
+}
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Week(num);
