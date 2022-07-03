1d3597e3bb8d7a3d62df32cbaa3b86dac58dbb19
Alay <alexeypekin13@gmail.com>
2022-07-03 20:42:52 +0300

    3-2

--- Domashka3Sem/Program.cs
+++ Domashka3Sem/Program.cs
@@ -15,3 +15,23 @@ int n = Convert.ToInt32(Console.ReadLine());
 Cube(n);
 
 //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void Palindrom(int num)
+{
+    
+    if(num >= 10000 && num < 100000)
+    {
+        int a = num / 10000;
+        int b = (num / 1000) % 10;
+        int c = (num / 10) % 10;
+        int d = num % 10;
+        if (a == d && b == c) Console.WriteLine("Число палиндром");
+        else
+        Console.WriteLine("Число не палиндром");
+    }
+    else 
+    Console.WriteLine("Number is uncorrect");
+}
+Console.WriteLine("Enter a five-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Palindrom(num);
