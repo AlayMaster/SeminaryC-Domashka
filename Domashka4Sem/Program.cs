b61eba415a762604537ebc780077c5c35cfd0926
Alay <alexeypekin13@gmail.com>
2022-07-04 23:18:32 +0300

    1

---
+++ Domashka4Sem/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumDigit( int a )
+{
+    int result = 0;
+    while (a > 0)
+    {
+     result = result + a % 10;
+     a = a / 10;
+    }
+    return result;   
+}
+
+Console.WriteLine("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Summa of digits {num} is {SumDigit(num)}");
