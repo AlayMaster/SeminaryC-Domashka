90b63a273e9715813db391c82c26512c632dcd8e
Alay <alexeypekin13@gmail.com>
2022-07-13 01:10:43 +0300

    6-2

--- Seminar6Domashka/Program.cs
+++ Seminar6Domashka/Program.cs
@@ -1,4 +1,5 @@
 ﻿// 1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
 int[] CreateArray(int size)
 {
     int[] newArray = new int[size];
@@ -34,3 +35,19 @@ int[] array1 = CreateArray(size);
 ShowArray(array1);
 int Positive = PositiveNumbers(array1);
 Console.WriteLine($"Positive numbers: " + Positive);
+
+// 2.  Напишите программу, которая найдёт точку пересечения двух прямых,заданных уравнениями 
+//     y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 -b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения ("+x+';'+y+')');
